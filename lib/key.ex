fedd2e06a1e2d10304347e6abc84bc0544066e74
chris@chriszimmerman.net <chris@chriszimmerman.net>
2018-05-21 20:29:54 -0400

    reorganize files and namespaces

--- lib/key.ex
+++ lib/key.ex
@@ -1,18 +1,45 @@
-defmodule Key do
-  @moduledoc """
-  Documentation for Key.
-  """
+defmodule Key.Key do
+  @notes [:a, :a_sharp, :b, :c, :c_sharp, :d, :d_sharp, :e, :f, :f_sharp, :g, :g_sharp]
 
-  @doc """
-  Hello world.
+  def get_major_key(note) when note in @notes do
+    cycle_to_base_note(note, @notes)
+    |> get_major_key_notes
+  end
 
-  ## Examples
+  def get_minor_key(note) when note in @notes do
+    cycle_to_base_note(note, @notes)
+    |> get_minor_key_notes
+  end
 
-      iex> Key.hello
-      :world
+  defp cycle_to_base_note(note, [base_note|rest]) when note != base_note do
+    cycle_to_base_note(note, rest ++ [base_note]) 
+  end
 
-  """
-  def hello do
-    :world
+  defp cycle_to_base_note(note, [base_note|rest]) when note == base_note do
+    [base_note|rest]
+  end
+
+  defp get_major_key_notes(notes) do
+    [
+      Enum.at(notes, 0),
+      Enum.at(notes, 2),
+      Enum.at(notes, 4),
+      Enum.at(notes, 5),
+      Enum.at(notes, 7),
+      Enum.at(notes, 9),
+      Enum.at(notes, 11)
+    ]
+  end
+
+  defp get_minor_key_notes(notes) do
+    [
+      Enum.at(notes, 0),
+      Enum.at(notes, 2),
+      Enum.at(notes, 3),
+      Enum.at(notes, 5),
+      Enum.at(notes, 7),
+      Enum.at(notes, 8),
+      Enum.at(notes, 10) 
+    ]
   end
 end
--- test/key_test.exs
+++ test/key_test.exs
@@ -1,8 +1,19 @@
 defmodule KeyTest do
   use ExUnit.Case
-  doctest Key
 
-  test "greets the world" do
-    assert Key.hello() == :world
+  test "get_major_key_a_returns_a_major_key" do
+    assert Key.Key.get_major_key(:a) == [:a, :b, :c_sharp, :d, :e, :f_sharp, :g_sharp]
+  end
+
+  test "get_major_key_a_sharp_returns_a_sharp_major_key" do
+    assert Key.Key.get_major_key(:a_sharp) == [:a_sharp, :c, :d, :d_sharp, :f, :g, :a]
+  end
+
+  test "get_minor_key_a_returns_a_minor_key" do
+    assert Key.Key.get_minor_key(:a) == [:a, :b, :c, :d, :e, :f, :g]
+  end
+
+  test "get_minor_key_a_sharp_returns_a_sharp_minor_key" do
+    assert Key.Key.get_minor_key(:a_sharp) == [:a_sharp, :c, :c_sharp, :d_sharp, :f, :f_sharp, :g_sharp]
   end
 end
